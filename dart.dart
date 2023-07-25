18b17636f5999da936a3c58bd55957d896339fcb
ca <139736019+sparkleicha@users.noreply.github.com>
2023-07-25 13:00:59 +0700

    Add files via upload
    tgs tgl 21-juli-2023

---
+++ dart.dart
@@ -0,0 +1,56 @@
+  void main() {
+  String namaDepan = "Aisyah";
+  String namaBelakang = " Salsabila";
+  String alamat = "JL HM SABAR rt 004 rw 001";
+  String telp = "0895358478085";
+  String namaLengkap = namaDepan + namaBelakang;
+  print('Nama: ' + namaLengkap);
+  print('Alamat: ' + alamat);
+  print('No.Telp: ' + telp);
+  
+  String spasi1 = "";
+  print(spasi1);
+  
+  int panjang = 5;
+  int lebar = 4;
+  int luas1 = panjang * lebar;
+  int sisi1 = 5;
+  int sisi2 = 5;
+  int luas2 = sisi1 * sisi2;
+  double setengah = 1/2;
+  int alas = 6;
+  int tinngi = 5;
+  double luas3 = setengah * alas * tinngi;
+  print('luas persegi panjang: panjang x lebar= 5 x 4 = ' + luas1.toString());
+  print('luas persegi: sisi x sisi= 5 x 5 = ' + luas2.toString());
+  print('luas segitiga: 1/2 x alas x tinngi= 1/2 x 6 x 5 = ' + luas3.toString());
+  
+   String spasi2 = "";
+  print(spasi2);
+  
+  int s1 = 5;
+  int s2 = 5;
+  int enam = 6;
+  int luas4 = enam * (s1 * s2);
+  print('luas permukaan kubus: 6(s x s)= 6(5 x 5) = ' + luas4.toString());
+  
+  int empat = 4;
+  double phi = 22/7;
+  int r1 = 7;
+  int r2 = 7;
+  double luas5 = empat * phi * r1 * r2;
+  print('luas permukaan bola: 4 x phi x r x r= 4 x 22/7 x 7 x 7 = ' + luas5.toString());
+
+  String spasi3 = "";
+  print(spasi3);
+  
+  int celcius = 40;
+  double rumus = (9/5 * celcius)+32;
+  print('celcius ke fahrenheit: (9/5 * celcius)+32= (9/5 * 40)+32= ' +rumus.toString());
+  
+  String spasi4 = "";
+  print(spasi4);
+  String rpl = "BERSAMA PA MUJAHID RPL SEMAKIN HEPII";
+  print(rpl);
+
+}
